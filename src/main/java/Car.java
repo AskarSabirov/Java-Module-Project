b1ee5db6d1638ba99d54a32439b4be7bbbe3fc79
AskarSabirov <sabirov_askar@inbox.ru>
2024-06-30 01:30:06 +0500

    ПР-1

---
+++ src/main/java/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    public static String car;
+    public static int speed;
+
+
+    public Car(String car, int speed) {
+        this.car = car;
+        this.speed = speed;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Введите название машины №" + i + ": ");
+            Car.car = scanner.next();
+
+            Car.speed = 0;
+            while (true) {
+                System.out.println("Введите скорость машины №" + i + ": ");
+                Car.speed = scanner.nextInt();
+                if ((Car.speed >= 0) & (Car.speed <= 250)) {
+                    break;
+                } else {
+                    System.out.println("Введите скорость от 0 до 250.");
+                }
+            }
+                Car car = new Car(Car.car, Car.speed);
+                Race.nowLead(car);
+
+        }
+
+
+        System.out.println("Победилем гонки становится: " + Race.lead);
+        scanner.close();
     }
 }
